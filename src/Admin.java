b7eb5bfb4b05aff840c64b006daf2dc434f259de
Kadiatou <kadiatou2021tall@gmail.com>
2025-06-09 17:47:01 +0000

    deuxième commit

--- src/Admin.java
+++ src/Admin.java
@@ -1,10 +1,24 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.*;
+
+
 public class Admin extends Employé{
-    public Admin(String prenom,String nom,String email, int tel){
+    private static final String FICHIER_JSON ="data.json";
+
+    public Admin(String prenom,String nom,String email, int tel)
+    {
         super(prenom,nom,email,tel);
     }
-    public void ajouterunemployé(){}
+    public void ajouterunemployé(String prenom,String nom){}
 
-    public void retirerunemployer(){}
+    public void retirerunemployer() throws FileNotFoundException {
+        BufferedReader read=new BufferedReader(new FileReader(FICHIER_JSON));
+
+        Type listtype = new TypeToken<List<Employé>>() {}.getType();
+    }
 
 
     public void vérifiersiunemployéestabonné(){}
